ccbd97eb1da98ae71b7d5630768eae7deee0119e
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-10-02 12:29:04 -0400

    MVP COMPLETED

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  // create elements
+  let div = document.createElement("div");
+  let span1 = document.createElement("span");
+  let h1 = document.createElement("h1");
+  let span2 = document.createElement("span");
+
+  //   add data and classnames to elements
+  div.classList.add("header");
+  span1.classList.add("date");
+  span1.textContent = "March 28, 2020";
+  h1.textContent = "Lambda Times";
+  span2.classList.add("temp");
+  span2.textContent = "98°";
+
+  //   create hierarchy among elements
+  div.appendChild(span1);
+  div.appendChild(h1);
+  div.appendChild(span2);
+
+  headerContainer = document.querySelector(".header-container");
+  headerContainer.appendChild(div);
+
+  return div;
+}
+Header();
